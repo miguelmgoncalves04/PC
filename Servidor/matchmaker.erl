602306af3b39b6ff6eea4830aef8c63afc8472f6
taveira60 <joaotaveira20051@gmail.com>
2026-04-22 15:36:27 +0100

    fixes matchmaker2

--- Servidor/matchmaker.erl
+++ Servidor/matchmaker.erl
@@ -24,23 +24,23 @@
 % acho que é isso xd
 
 start() ->
-    spawn(fun() -> loop([], #{}) end).
+    spawn(fun() -> loop([], [], #{}) end).
 
 
 
-join_queue(Pid, Username) ->  % PID: SERVER_PID
-    Pid ! {join_queue, self(), Username},
+join_queue(Server_Pid, Username) ->  % PID: SERVER_PID
+    Server_Pid ! {join_queue, self(), Username},
     receive Response -> Response end.
 
-leave_queue(Pid, Username) ->  % PID: SERVER_PID
-    Pid ! {leave_queue, self(), Username},
+leave_queue(Server_Pid, Username) ->  % PID: SERVER_PID
+    Server_Pid ! {leave_queue, self(), Username},
     receive Response -> Response end.
 
 loop(QueueNames, QueuePids, Games) ->
     receive
         % Jogador entra na fila
         {join_queue, From, Username} ->
-            case lists:member(Username,Queue) of % if username in queue
+            case lists:member(Username,QueueNames) of % if username in queue
                 true ->
                     From ! {error, already_in_queue},
                     loop(QueueNames, QueuePids, Games);
@@ -48,8 +48,8 @@ loop(QueueNames, QueuePids, Games) ->
                     NewNames = QueueNames ++ [Username],
                     NewPids  = QueuePids ++ [From],
                     From ! ok,
-                    {FinalQueue, FinalGames} = start_game(NewQueue, Games),  %[TRIGGER] pode acontecer um novo jogo
-                    loop(FinalQueue, FinalGames)
+                    {FinalQueue,FinalPids ,FinalGames} = start_game(NewNames, NewPids,Games),  %[TRIGGER] pode acontecer um novo jogo
+                    loop(FinalQueue, FinalPids, FinalGames)
             end;
         %Conas sai da fila
         {leave_queue, From, Username} ->
@@ -65,44 +65,63 @@ loop(QueueNames, QueuePids, Games) ->
         %Avisar o mastchmaker que um jogo terminou, ou seja no caso de isto estar cheio pode voltar a tentar encher um servidor
         {game_finished, GameId} ->
             NewGames = maps:remove(GameId, Games),
-            {FinalQueue, FinalGames} = start_game(Queue, NewGames), %[TRIGGER] pode acontecer um novo jogo
-            loop(FinalQueue, FinalGames)
+            {FinalQueue, FinalPids, FinalGames} = start_game(QueueNames, QueuePids ,NewGames), %[TRIGGER] pode acontecer um novo jogo
+            loop(FinalQueue,FinalPids, FinalGames)
     end.
 
 
 
 
 %Começar jogos
-start_game(Queue, Games) ->
-    case {length(Queue) >= 3, maps:size(Games) < 4} of % min de jogadores: 3 e max de salas: 4
+start_game(QueueNames,QueuePids,Games) ->
+    case {length(QueueNames) >= 3, maps:size(Games) < 4} of % min de jogadores: 3 e max de salas: 4
 
         {true, true} ->
             N = case length(QueueNames) >= 4 of true -> 4; false -> 3 end,
+
             SelectedNames = lists:sublist(QueueNames, N), 
             SelectedPids  = lists:sublist(QueuePids, N), 
+
             RestNames = lists:nthtail(N, QueueNames), 
             RestPids  = lists:nthtail(N, QueuePids), 
 
-            %depois há que ligar isto ao cenas que vai fazer a comunicação entre o client e o servidor
-            io:format("Novo jogo encontrado (~p) com jogadores: ~p~n", [GameId, Players]),
+            GameId = make_ref(),
 
-            NewGames = maps:put(GameId, Players, Games),
+            %depois há que ligar isto ao cenas que vai fazer a comunicação entre o client e o servidor
+            io:format("Novo jogo encontrado (~p) com jogadores: ~p~n", [GameId, SelectedNames]),
+
+            NewGames = maps:put(GameId, SelectedPids, Games),
 
             %tenta criar mais jogos recursivamente
-            start_game(RestQueue, NewGames);
+            start_game(RestNames, RestPids ,NewGames);
 
         _ ->
-            {Queue, Games}
+            {QueueNames, QueuePids,Games}
     end.
 
 
 
 
 %Escolher jogadores para entrarem no mapa
-take_players(Queue) -> %isto aqui entra sempre quando a condiçao de start_game é TRUE
-    case length(Queue) >= 4 of %isto ta meio javardo mas funciona bem
+%pick player to play
+% pptp(Queue) -> %isto aqui entra sempre quando a condiçao de start_game é TRUE
+%     case length(Queue) >= 4 of %isto ta meio javardo mas funciona bem
+%         true ->
+%             {lists:sublist(Queue, 4), lists:nthtail(4, Queue)};
+%         false ->
+%             {lists:sublist(Queue, 3), lists:nthtail(3, Queue)}
+%     end.
+
+% nome precisa de ser mudado
+remove_player(Username, Names, Pids) ->
+    remove_player(Username, Names, Pids, [], []).
+
+remove_player(_, [], [], NewNames, NewPids) ->
+    {lists:reverse(NewNames), lists:reverse(NewPids)};
+remove_player(Username, [Name|RestNames], [Pid|RestPids], AccNames, AccPids) ->
+    if
+        Name =:= Username ->
+            remove_player(Username, RestNames, RestPids, AccNames, AccPids);
         true ->
-            {lists:sublist(Queue, 4), lists:nthtail(4, Queue)};
-        false ->
-            {lists:sublist(Queue, 3), lists:nthtail(3, Queue)}
-    end.
+            remove_player(Username, RestNames, RestPids, [Name|AccNames], [Pid|AccPids])
+    end. 
