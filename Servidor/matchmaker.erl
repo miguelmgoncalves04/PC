4d7423f4d18e8787174f180a87f8dec924414a51
taveira60 <joaotaveira20051@gmail.com>
2026-04-29 18:44:25 +0100

    matchmaker melhorado mais clean e sem merdas desnecessarias

--- Servidor/matchmaker.erl
+++ Servidor/matchmaker.erl
@@ -1,110 +1,73 @@
 -module(matchmaker).
--export([start/0, join_queue/2, leave_queue/2]).
-
-%ALTERAÇÕES:
-
-%alteração linha 81 á 90
-
-%unica coisa que mudei foi outra vez a cena do GameId = make_ref() eu comentei o meu pensamento, caso eu estja simplesmente a 
-%pensar d+ chamem-me de burro mas digam-me porque é que é melhor da outra forma kkkk
-% ahh e como game_session:start pelo que eu entendi do comdigo acho que foi o miguel usa um par {Username, Pid} usei o zip e chamei
-% o start simplesmente, mas nem pensei muito nem sei se é suposto ser assim
-% acho que foi so isso as alterações
+-export([start/0]).
 
 start() ->
-    spawn(fun() -> loop([], [], #{}) end).
-
-
-
+    spawn(fun() -> loop([], #{}) end).
 % PID: Matchmaker_PID
 
 
-loop(QueueNames, QueuePids, Games) ->
+loop(QNamesPids , Games) ->
     receive
         % Jogador entra na fila
         {join_queue, From, Username} ->
             % if username in queue
-            case lists:member(Username, QueueNames) of
+            case lists:any(fun({_,Pid})-> Pid =:= From end , QNamesPids) of
                 true ->
                     From ! {error, already_in_queue},
-                    loop(QueueNames, QueuePids, Games); 
+                    loop(QNamesPids, Games); 
                 false ->
-                    NewNames = QueueNames ++ [Username],
-                    NewPids = QueuePids ++ [From],
+                    NewQueue = QNamesPids ++ [{Username,From}],
                     From ! ok,
                     %[TRIGGER] pode acontecer um novo jogo
-                    {FinalQueue, FinalPids, FinalGames} = start_game(NewNames, NewPids, Games),
-                    loop(FinalQueue, FinalPids, FinalGames)
+                    {FinalQueue, FinalGames} = start_game(NewQueue, Games),
+                    loop(FinalQueue, FinalGames)
             end;
         %Conas sai da fila
-        {leave_queue, From, Username} ->
-            case lists:member(Username, QueueNames) of
+        {leave_queue, From, _} ->
+            case lists:any(fun({_,Pid})-> Pid =:= From end , QNamesPids) of
                 true ->
-                    {NewNames, NewPids} = remove_player(Username, QueueNames, QueuePids),
+                    NewQueue = lists:filter(fun({_,Pid})-> Pid =/= From end,QNamesPids),
                     From ! ok,
-                    loop(NewNames, NewPids, Games);
+                    loop(NewQueue, Games);
                 false ->
                     From ! ok,
-                    loop(QueueNames, QueuePids, Games)
+                    loop(QNamesPids, Games)
             end;
         %Avisar o mastchmaker que um jogo terminou, ou seja no caso de isto estar cheio pode voltar a tentar encher um servidor
         {game_finished, GameId} ->
             NewGames = maps:remove(GameId, Games),
             %[TRIGGER] pode acontecer um novo jogo
-            {FinalQueue, FinalPids, FinalGames} = start_game(QueueNames, QueuePids, NewGames),
-            loop(FinalQueue, FinalPids, FinalGames)
+            {FinalQueue, FinalGames} = start_game(QNamesPids, NewGames),
+            loop(FinalQueue, FinalGames)
     end.
 
 %Começar jogos
-start_game(QueueNames,QueuePids,Games) ->
-    case {length(QueueNames) >= 3, maps:size(Games) < 4} of % min de jogadores: 3 e max de salas: 4
+start_game(QNamesPids,Games) ->
+    case {length(QNamesPids) >= 3, maps:size(Games) < 4} of % min de jogadores: 3 e max de salas: 4
 
         {true, true} ->
-            N = case length(QueueNames) >= 4 of true -> 4; false -> 3 end,
+            N = case length(QNamesPids) >= 4 of true -> 4; false -> 3 end,
 
-            SelectedNames = lists:sublist(QueueNames, N), 
-            SelectedPids  = lists:sublist(QueuePids, N), 
+            SelectedPlayers = lists:sublist(QNamesPids, N),  
+            RestPlayers = lists:nthtail(N, QNamesPids), 
 
-            RestNames = lists:nthtail(N, QueueNames), 
-            RestPids  = lists:nthtail(N, QueuePids), 
-            
-            Players = lists:zip(SelectedNames, SelectedPids),
-            GamePid = game_session:start(Players, self()),
+            GamePid = game_session:start(SelectedPlayers, self()),
 
             % GameId = make_ref(), tinha isto dantes mas acho melhor usar o GamePid simpelsmente porque depois vamos ter problemas ao remover a partida do mapa. Tipo o game_session vai mandar um {game_finished, self()} (acho sem ver) e aqui o self vai ser o GamePid.E como a chave do mapa é o GamePid assim remove diretamente, se fosse um makeref() seria outro pid e acho que iam tipo encher que no caso acho que pelo enunciado so pode ter 4 partidas ou algo assim. Por isso mudei isso mas posso so tar a trollar mb se tiver mas assim é 100% que funciona.
             
             %depois há que ligar isto ao cenas que vai fazer a comunicação entre o client e o servidor 
+            SelectedPids=[Pid || {_,Pid}<-SelectedPlayers],
             [Pid ! {matchmaker, {game_start, GamePid}} || Pid <- SelectedPids],
+            SelectedNames = [Name || {Name, _} <- SelectedPlayers],
             io:format("Novo jogo (~p) com jogadores: ~p~n", [GamePid, SelectedNames]),
 
-            NewGames = maps:put(GamePid, SelectedPids, Games),
+            NewGames = maps:put(GamePid, SelectedPlayers, Games),
 
             %tenta criar mais jogos recursivamente
-            start_game(RestNames, RestPids ,NewGames);
+            start_game(RestPlayers ,NewGames);
         _ ->
-            {QueueNames, QueuePids, Games}
+            {QNamesPids, Games}
     end.
 
-%Escolher jogadores para entrarem no mapa
-%pick player to play
-% pptp(Queue) -> %isto aqui entra sempre quando a condiçao de start_game é TRUE
-%     case length(Queue) >= 4 of %isto ta meio javardo mas funciona bem
-%         true ->
-%             {lists:sublist(Queue, 4), lists:nthtail(4, Queue)};
-%         false ->
-%             {lists:sublist(Queue, 3), lists:nthtail(3, Queue)}
-%     end.
 
-% nome precisa de ser mudado
-remove_player(Username, Names, Pids) ->
-    remove_player(Username, Names, Pids, [], []).
 
-remove_player(_, [], [], NewNames, NewPids) ->
-    {lists:reverse(NewNames), lists:reverse(NewPids)};
-remove_player(Username, [Name | RestNames], [Pid | RestPids], AccNames, AccPids) ->
-    if
-        Name =:= Username ->
-            remove_player(Username, RestNames, RestPids, AccNames, AccPids);
-        true ->
-            remove_player(Username, RestNames, RestPids, [Name | AccNames], [Pid | AccPids])
-    end.
